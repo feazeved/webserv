1939dbc46ccdd59c4e8abfbec776d3fdce3fc787
Alex <alexdeimling@gmail.com>
2026-07-21 16:40:15 +0100

    Created Block Vector

---
+++ src/BlockVector.hpp
@@ -0,0 +1,59 @@
+#include "core.hpp"
+#include <new>
+#include <unistd.h>
+
+// Class template 
+template <typename Type, usize blockSize, usize maxBlocks>
+class BlockVector
+{
+public:
+	Type stack[blockSize];
+	Type* blocks[maxBlocks];
+	usize blockCount;
+
+	BlockVector() : blockCount(1)
+	{
+		blocks[0] = stack;
+
+		for (usize i = 1; i < maxBlocks; i++)
+			blocks[i] = NULL;
+	}
+
+	~BlockVector()
+	{
+		for (usize i = 1; i < blockCount; i++)
+			delete[] blocks[i];
+	}
+
+	bool grow()
+	{
+		if (blockCount >= maxBlocks)
+			return false;
+		Type* block = new (std::nothrow) Type[blockSize];
+		if (block == NULL)
+			return false;
+		blocks[blockCount] = block;
+		blockCount++;
+		return true;
+	}
+
+	bool shrink()
+	{
+		if (blockCount <= 1)
+			return false;
+		blockCount--;
+		delete[] blocks[blockCount];
+		blocks[blockCount] = NULL;
+		return true;
+	}
+
+	Type& operator[](usize index)
+	{
+		return blocks[index / blockSize][index % blockSize];
+	}
+
+	const Type& operator[](usize index) const
+	{
+		return blocks[index / blockSize][index % blockSize];
+	}
+};
