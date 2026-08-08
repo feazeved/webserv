132e4024b133631182ecbe71937cd2fb6d23cf04
wlucas-f <wallacelfgoncalves@gmail.com>
2026-08-08 22:27:19 +0100

    added cgi-bin

---
+++ www/cgi-bin/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head')
+print ('<title>Hello, World! - Yes, this is a valid CGI</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello, World! THIS IS VALID, DONT BOTHER</h2>')
+print ('</body>')
+print ('</html>')
